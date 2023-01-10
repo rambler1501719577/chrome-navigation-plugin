ee8900f20c8c5ffff6a60cc2cda4fac3caedb5c6
rambler1501719577 <1501719577@qq.com>
2023-01-10 09:29:58 +0800

    调整项目结构

--- favicon-server/lib/pug-filter-markdown-it-prism.js
+++
@@ -1,8 +0,0 @@
-const MarkdownIt = require('markdown-it');
-const prism = require('markdown-it-prism');
-
-module.exports = (markdown, options) => {
-  const md = new MarkdownIt(options);
-  md.use(prism, options.prism);
-  return md.render(markdown);
-};
